d9dbfe093a29d9c8815f815e305dac3824748dcb
SuvarnaZ1990 <30748623+SuvarnaZ1990@users.noreply.github.com>
2025-05-25 21:53:36 +0000

    Variables and constant

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -3,7 +3,10 @@ let accountEmail = "email@gmail.com"
 var accountPassword = "12345"
 accountCity = "Mumbai"
 let accountState;
-
+/*
+Prefer not to use var instead us let 
+because of the issue in block scope and functional scope
+*/
 //accountId = 2  //not allowed
 accountEmail = "Changeemail@gmail.com"
 accountPassword = "Change12345"
