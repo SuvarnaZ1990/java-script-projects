e217efb9f929d1b8b8fdf36e649cfeaacfaeb2f8
SuvarnaZ1990 <30748623+SuvarnaZ1990@users.noreply.github.com>
2025-05-25 21:50:08 +0000

    Variables and constants

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "email@gmail.com"
+var accountPassword = "12345"
+accountCity = "Mumbai"
+let accountState;
+
+//accountId = 2  //not allowed
+accountEmail = "Changeemail@gmail.com"
+accountPassword = "Change12345"
+accountCity = "ChangeMumbai"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
